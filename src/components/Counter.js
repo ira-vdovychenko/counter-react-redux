86183755ea09df5bed90496e7810763be13d2882
Ira <vdovychenko.ire@gmail.com>
2023-05-26 11:18:56 +0300

    added changes

---
+++ src/components/Counter.js
@@ -0,0 +1,40 @@
+/* import { connect } from "react-redux"; */
+import {inc, dec, rnd} from '../actions'
+/* import { Component } from "react"; */
+import { useSelector, useDispatch } from 'react-redux'
+
+ const Counter = () => {
+
+  const counter = useSelector(state => state.counter);
+  const dispatch = useDispatch();
+
+    return (
+        <div className="jumbotron">
+      <h1>{counter}</h1>
+      <button onClick={() => dispatch(dec())} className="btn btn-primary">DEC</button>
+      <button onClick={() => dispatch(inc())} className="btn btn-primary">INC</button>
+      <button onClick={() => dispatch(rnd())} className="btn btn-primary">RND</button> 
+    </div>
+    )
+} 
+/* class Counter extends Component {
+  render() {
+    const {counter, inc, dec, rnd} = this.props;
+    return (
+      <div className="jumbotron">
+    <h1>{counter}</h1>
+    <button onClick={dec} className="btn btn-primary">DEC</button>
+    <button onClick={inc} className="btn btn-primary">INC</button>
+    <button onClick={rnd} className="btn btn-primary">RND</button>
+  </div>
+  )
+  }
+}
+const mapStateToProps = (state) => {
+  return {
+    counter: state.value
+  }
+}
+
+export default connect(mapStateToProps, actions)(Counter); */
+export default Counter;
---
+++ src/reducer.js
@@ -0,0 +1,25 @@
+const initialState = {counter: 0};//зелений квадратик
+
+const reducer = (state = initialState, action) => {//(як setState)червоний квадратик, який буде якимись методами оновлювати стейт (зелений квадратик)
+    switch (action.type) {
+      case 'INC':
+        return {
+          ...state,
+          counter: state.counter + 1
+        }; 
+      case 'DEC':
+        return {
+          ...state,
+          counter: state.counter - 1
+        }; 
+      case 'RND':
+          return {
+            ...state,
+            counter: state.counter * action.payload
+          };
+        default: 
+        return state;
+    }
+}
+
+export default reducer;
